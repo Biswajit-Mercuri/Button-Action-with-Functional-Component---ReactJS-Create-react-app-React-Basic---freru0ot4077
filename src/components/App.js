a6456b5b5124041bc411afcdf1de6cd81f7516f5
mercuri <mercuri90028@gmail.com>
2022-10-05 23:46:57 +0530

    done we can run now

--- src/components/App.js
+++ src/components/App.js
@@ -2,9 +2,18 @@ import React, {Component, useState} from "react";
 import "./../styles/App.css";
 
 function App() {
+
+  const [show,setShow] = useState(true);
+
   return (
     <div id="main">
-      // Do not alter the main div
+      <button id="click" onClick={()=>{
+        setShow(false)
+      }}>
+        Show
+      </button>
+
+      {show? null : <p id="para">Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy</p>}
     </div>
   );
 }
